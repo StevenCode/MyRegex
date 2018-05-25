57f5791bd766cdf0edc86e8c9c1b7b420e4ff9e7
admin <admin>
2018-05-25 10:58:58 +0800

    简易正则表达式完成

--- src/com/steven/Main.java
+++ src/com/steven/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String s = "ab+c";
-        String aa = "ababc";
+        String aa = "ababca";
 
         Pattern compile = Pattern.compile(s);
         boolean match = compile.match(aa);
